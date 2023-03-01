f82b562c14b5f4866640134a47046490d6c3f344
huytd <huytd@nal.vn>
2023-03-01 14:48:28 +0700

    uninstall old app

--- lib/screens/splash_screens.dart
+++ lib/screens/splash_screens.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:base_project/config/routes.dart';
 import 'package:base_project/generated/assets.dart';
 import 'package:base_project/services/local/app_share.dart';
+import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,14 +14,36 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final oldAppPackageName = 'vn.etelecom.njvcall';
+
   @override
   void initState() {
     super.initState();
+    onInit();
+
     Future.delayed(
         const Duration(seconds: 2),
         () => Get.offAllNamed(AppShared.isAutoLogin == "false"
             ? Routes.loginScreen
             : Routes.homeScreen));
+  }
+
+  onInit() async {
+    retryUnInstallOldApp(false);
+  }
+
+  retryUnInstallOldApp(bool isUnInstalled) async {
+    if (isUnInstalled) {
+      return;
+    } else {
+      await DeviceApps.uninstallApp(oldAppPackageName);
+
+      var oldAppStillExisted =
+          await DeviceApps.isAppInstalled(oldAppPackageName);
+      if (oldAppStillExisted) {
+        retryUnInstallOldApp(false);
+      }
+    }
   }
 
   @override
