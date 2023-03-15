5919038be5d6e6874ea183df039da62207ef77f6
Phạm Thái Công <73935138+congcong1998@users.noreply.github.com>
2023-03-15 15:18:19 +0700

    [FIX BUG] Fix phone button effect

--- lib/common/widget/button_phone_custom_widget.dart
+++ lib/common/widget/button_phone_custom_widget.dart
@@ -23,3 +23,67 @@ class ButtonPhoneCustomWidget extends StatelessWidget {
     );
   }
 }
+
+
+class AnimatedPhoneButton extends StatefulWidget {
+  final String text;
+  final Function onPressed;
+
+  const AnimatedPhoneButton({Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _AnimatedButtonState();
+}
+
+class _AnimatedButtonState extends State<AnimatedPhoneButton> {
+  bool _isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        setState(() {
+          _isPressed = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          _isPressed = false;
+        });
+        widget.onPressed();
+      },
+      onTapCancel: () {
+        setState(() {
+          _isPressed = false;
+        });
+      },
+      child: AnimatedContainer(
+        width: 80,
+        height: 80,
+        duration: const Duration(milliseconds: 150),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(45),
+          color: Colors.white,
+          border: Border.all(
+            color: _isPressed ? Colors.red : Colors.transparent,
+            width: 1,
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Center(
+          child: Text(
+            widget.text,
+            style: FontFamily.demiBold(size: 32, color: AppColor.colorBlack)
+            // const TextStyle(
+            //   fontSize: 32,
+            //   fontFamily: FontFamily.fontFamily,
+            //   color: AppColor.colorBlack,
+            // ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
