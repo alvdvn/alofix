6fe2811c602715f447948b03049cb5e867f8c27d
Tuan Anh Nguyen <>
2023-10-12 11:29:26 +0700

    update code api version

--- android/app/src/main/kotlin/com/example/base_project/AppInstance.kt
+++ android/app/src/main/kotlin/com/example/base_project/AppInstance.kt
@@ -9,6 +9,6 @@ object AppInstance {
    const val baseURL :String = "alo.njv.vn"
    const val callLogURL :String = "https://"+ baseURL + "/api/calllogs"
 
-   const val API_VERSION :String = "9"
+   const val API_VERSION :String = "10"
 
 }
