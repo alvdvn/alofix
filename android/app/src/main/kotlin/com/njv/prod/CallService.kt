926ca8f052f037acd5ebbead2d295869ec955ddc
Tuan Anh Nguyen <>
2023-12-20 09:24:40 +0700

    update code wakelook call service

--- android/app/src/main/kotlin/com/njv/prod/CallService.kt
+++ android/app/src/main/kotlin/com/njv/prod/CallService.kt
@@ -1,6 +1,9 @@
 package com.njv.prod
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.os.Build
+import android.os.PowerManager
 import android.telecom.Call
 import android.telecom.CallAudioState
 import android.telecom.InCallService
@@ -11,6 +14,9 @@ import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instanc
 class CallService : InCallService() {
 
     private var instance: CallService? = null
+    private var wakeLock: PowerManager.WakeLock? = null
+    private var keyguardLock: KeyguardManager.KeyguardLock? = null
+    private val tag = AppInstance.TAG
 
     override fun onCreate() {
         super.onCreate()
@@ -19,11 +25,47 @@ class CallService : InCallService() {
 
     override fun onCallAdded(call: Call) {
         OngoingCall.call = call
+        // Acquire wake lock to wake up the device
+        acquireWakeLock();
+        // Disable the keyguard to turn on the screen
+        disableKeyguard();
         CallActivity.start(this, call)
     }
 
     override fun onCallRemoved(call: Call) {
         OngoingCall.call = null
+        // Release the wake lock when the call is disconnected
+        releaseWakeLock();
+        // Re-enable the keyguard
+        enableKeyguard();
+    }
+
+    private fun acquireWakeLock() {
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager?
+        if (powerManager != null) {
+            wakeLock = powerManager.newWakeLock(
+                PowerManager.FULL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                "$tag::InCallWakeLock"
+            )
+            wakeLock?.acquire()
+        }
+    }
+    private fun releaseWakeLock() {
+        if (wakeLock?.isHeld == true) {
+            wakeLock?.release()
+            wakeLock = null
+        }
+    }
+    private fun disableKeyguard() {
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager?
+        if (keyguardManager != null) {
+            keyguardLock = keyguardManager.newKeyguardLock(tag)
+            keyguardLock?.disableKeyguard()
+        }
+    }
+    private fun enableKeyguard() {
+        keyguardLock?.reenableKeyguard()
+        keyguardLock = null
     }
 
     companion object {
