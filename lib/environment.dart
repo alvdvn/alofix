de0b145ba5dd8ce3f9b3b88d9c0839f9f5ecc96e
DONGNH_DT <alvdvip@gmail.com>
2024-02-01 07:54:21 +0700

    rollback evironment

--- lib/environment.dart
+++ lib/environment.dart
@@ -3,7 +3,7 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 class Environment {
   static  PackageInfo? _packageInfo;
-  static const _isReleaseMode = false;
+  static const _isReleaseMode = kReleaseMode || kProfileMode;
   static late String _apiDomain;
 
   static bool isProduction() {
@@ -35,7 +35,6 @@ class Environment {
   static String get apiDomain {
     if (_isReleaseMode) {
       return 'https://alonjv-fix-dblocal.njv.vn';
-      // return 'https://alonjv-fix-invalid-calllog.njv.vn';
     }
     return _apiDomain;
   }
